5010a41b1b6a65a4efe33bd3fff1dc617150f5b1
cizal <cizal@esignature.com.np>
2023-10-13 00:23:08 +0545

    Made changes to the Home Tab

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -8,7 +8,43 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(children: [HomePageCarousel(), CardsScroll()]),
+      child: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            "Featured".toUpperCase(),
+            style: TextStyle(
+                color: Colors.grey[700], fontSize: 20.0, letterSpacing: 2.0),
+          ),
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        HomePageCarousel(),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.arrow_upward,
+                color: Colors.red,
+              ),
+              const SizedBox(
+                width: 2.0,
+              ),
+              Text(
+                "Trending".toUpperCase(),
+                style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 20.0,
+                    letterSpacing: 2.0),
+              ),
+            ],
+          ),
+        ),
+        CardsScroll(),
+      ]),
     );
   }
 }
