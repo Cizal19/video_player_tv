59d876707a0897b88bfcb38b3d53eacf085fcb0f
cizal <cizal@esignature.com.np>
2023-10-12 17:21:35 +0545

    Created new Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:video_player_tv/screens/ChannelList.dart';
+import 'package:video_player_tv/screens/Home.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: ChannelList(),
+    home: Home(),
     theme: ThemeData(
         appBarTheme: AppBarTheme(
       color: Color(0xffffd700),
