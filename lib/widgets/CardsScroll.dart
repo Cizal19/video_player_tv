4f8b6f8ef01b17741a99e10b95ebe05e5872d2ad
cizal <cizal@esignature.com.np>
2023-10-17 21:14:32 +0545

    Made Cards Focusable and Clickable

--- lib/widgets/CardsScroll.dart
+++ lib/widgets/CardsScroll.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:video_player_tv/models/Channel.dart';
 import 'package:video_player_tv/models/channels.api.dart';
+import 'package:video_player_tv/screens/VideoPlayerScreen.dart';
 
 class CardsScroll extends StatefulWidget {
   const CardsScroll({super.key});
@@ -36,38 +38,59 @@ class _CardsScrollState extends State<CardsScroll> {
           } else if (snapshot.hasData) {
             return Container(
               height: 200.0,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                shrinkWrap: true,
-                itemCount: list.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    width: 250.0, // Adjust card width as needed
-                    // margin: EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Card(
-                      elevation: 4.0,
-                      child: Column(
-                        children: <Widget>[
-                          Image.network(
-                            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/${list[index].thumb}",
-                            fit: BoxFit.cover,
-                            height: 150.0,
-                            width: double.infinity,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              list[index].title,
-                              style: TextStyle(
-                                fontSize: 16.0,
-                              ),
+              child: FocusScope(
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemCount: list.length,
+                  itemBuilder: (context, index) {
+                    return Focus(
+                      focusNode: FocusNode(),
+                      onKey: (node, event) {
+                        if (event is RawKeyDownEvent) {
+                          if (event.logicalKey == LogicalKeyboardKey.select) {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return VideoPlayerScreen(
+                                  videoUrl: list[index].sources[0]);
+                            }));
+                          }
+                        }
+                        return KeyEventResult.ignored;
+                      },
+                      child: InkWell(
+                        onTap: () {},
+                        focusColor: Colors.blue,
+                        child: Container(
+                          width: 250.0, // Adjust card width as needed
+                          // margin: EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Card(
+                            elevation: 4.0,
+                            child: Column(
+                              children: <Widget>[
+                                Image.network(
+                                  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/${list[index].thumb}",
+                                  fit: BoxFit.cover,
+                                  height: 150.0,
+                                  width: double.infinity,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    list[index].title,
+                                    style: TextStyle(
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ],
+                        ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             );
           } else {
